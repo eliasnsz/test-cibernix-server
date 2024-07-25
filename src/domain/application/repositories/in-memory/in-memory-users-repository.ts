66b68acdc3aa4ce4847f40a2a86211dd2f427c0a
eliasnsz <eliasnsz.dev@gmail.com>
2024-07-25 20:04:02 -0300

    :sparkles: feat: add users in-memory repository

---
+++ src/domain/application/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,14 @@
+import type { User } from "../../../enterprise/entities/user";
+import type { UsersRepository } from "../users-repository";
+
+export class InMemoryUsersRepository implements UsersRepository {
+	public users: User[] = [];
+
+	async create(user: User) {
+		this.users.push(user);
+	}
+
+	async findByEmail(email: string) {
+		return this.users.find((user) => user.email === email) ?? null;
+	}
+}
