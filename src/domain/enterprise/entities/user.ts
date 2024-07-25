b118a39edc48a11ca7decd3e692dbc6acfb54ddb
eliasnsz <eliasnsz.dev@gmail.com>
2024-07-25 19:59:15 -0300

    :sparkles: feat: add user entity

---
+++ src/domain/enterprise/entities/user.ts
@@ -0,0 +1,43 @@
+import { randomUUID } from "node:crypto";
+import type { Optional } from "../../types/optional";
+
+interface UserProps {
+	id: string;
+	name: string;
+	email: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export class User {
+	private props: UserProps;
+
+	get id() {
+		return this.props.id;
+	}
+
+	get name() {
+		return this.props.name;
+	}
+
+	get email() {
+		return this.props.email;
+	}
+
+	get createdAt() {
+		return this.props.createdAt;
+	}
+
+	get updatedAt() {
+		return this.props.updatedAt;
+	}
+
+	constructor(props: Optional<UserProps, "id" | "createdAt" | "updatedAt">) {
+		this.props = {
+			...props,
+			id: props.id ?? randomUUID(),
+			createdAt: props.createdAt ?? new Date(),
+			updatedAt: new Date(),
+		};
+	}
+}
